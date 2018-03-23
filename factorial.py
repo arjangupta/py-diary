13b272c3cc554b20bc2e4460e6b0280f0903afd4
Arjan Gupta <arjan.gupta@eyeverify.com>
2018-03-23 18:25:28 -0500

    Add descriptive comments

--- factorial.py
+++ factorial.py
@@ -5,12 +5,11 @@
 
 # Only calculates for positive integers
 def calc_factorial(n):
-	# Stop recursing when n is 1
-	if n < 0:
+	if n < 0: # bad input
 		return "not computable because the number is negative"
-	elif n == 0 or n == 1:
+	elif n == 0 or n == 1: # stop recursing when n is 1 or 0
 		return 1
-	else:
+	else: # resursion rule
 		return n * calc_factorial(n-1)
 
 num = 5
