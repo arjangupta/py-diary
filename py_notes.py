1f2e1f4590ac7cdd8e9dc4d34cfb09756bace397
Arjan Gupta <arjan.gupta@eyeverify.com>
2018-03-31 14:48:10 -0500

    Add multi-line comments in py_notes

---
+++ py_notes.py
@@ -0,0 +1,18 @@
+"""
+	Created by Arjan Gupta on 3/31/2018
+	Description: Useful notes in python
+"""
+
+# Multi-line strings for multi-line output
+
+sample_address = """
+This is the a sample address:
+1234 Hello World St
+Apt 987
+Kansas City, MO
+XXXXX
+USA
+"""
+
+print sample_address
+
