ba8c9baba761eda19e142910460e44c23b20ae91
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-05-07 17:39:40 -0500

    Call function, rotate each char and print line by line

--- small-programs/rotate_string.py
+++ small-programs/rotate_string.py
@@ -1,4 +1,5 @@
 def rotate_by_n_chars(string, n_rotations):
-    print(chr(ord(string[n_rotations])+2))
-
-rotate_by_n_chars("Hello", 2)
+    for char in string:
+        print(chr(ord(char) + n_rotations))
+ 
+rotate_by_n_chars("Hello", -2)
