a7a2555c6f11afe2658f9a7a485d0c77b80a449a
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-05-07 17:57:07 -0500

    Finish implementing working char rotate function

--- small-programs/rotate_string.py
+++ small-programs/rotate_string.py
@@ -1,5 +1,10 @@
 def rotate_by_n_chars(string, n_rotations):
+    resultant_string = ""
     for char in string:
-        print(chr(ord(char) + n_rotations))
- 
-rotate_by_n_chars("Hello", -2)
+        resultant_string += str(chr(ord(char) + n_rotations))
+    return resultant_string
+
+string_to_be_rotated = "Fcjjm"
+n_rotations = 2
+print(f"The rotated form of {string_to_be_rotated} is {rotate_by_n_chars(string_to_be_rotated, n_rotations)}")
+print(f"The sring was rotated by {n_rotations}")
