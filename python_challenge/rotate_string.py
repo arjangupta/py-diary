10300c4d8042e7c22f928fe07c7f562ca0d1148f
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-05-09 16:18:25 -0500

    Replace all double quotes with single quotes

--- python_challenge/rotate_string.py
+++ python_challenge/rotate_string.py
@@ -1,8 +1,8 @@
 def rotate_by_n_chars(string, n_rotations):
-    resultant_string = ""
+    resultant_string = ''
     for char in string:
-        if char == " ":
-            resultant_string += " "
+        if char == ' ':
+            resultant_string += ' '
         else:
             raw_rotation = ord(char) + n_rotations
             adjusted_rotation = 0
@@ -15,13 +15,13 @@ def rotate_by_n_chars(string, n_rotations):
             resultant_string += str(chr(adjusted_rotation))
     return resultant_string
 
-string_to_be_rotated = ""
-with open("problem2_encrypted_message.txt") as file:
+string_to_be_rotated = ''
+with open('problem2_encrypted_message.txt') as file:
     string_to_be_rotated = file.read()
 
 n_rotations = 2
-print(f"The rotated form of\n\n{string_to_be_rotated}\n\nis\n\n{rotate_by_n_chars(string_to_be_rotated, n_rotations)}")
-print(f"\nThe sring was rotated by {n_rotations}")
+print(f'The rotated form of\n\n{string_to_be_rotated}\n\nis\n\n{rotate_by_n_chars(string_to_be_rotated, n_rotations)}')
+print(f'\nThe sring was rotated by {n_rotations}')
 
 # 97 to 122 is lowercase letter ASCII
 # 65 to 90  is uppercase letter ASCII
