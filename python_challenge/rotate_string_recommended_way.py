373638bc57d14f7a677d6251e0a06e8f7d1e3a7c
Arjan Gupta <arjangupta95@gmail.com>
2019-05-14 10:09:23 -0500

    Remove extraneous comments

--- python_challenge/rotate_string_recommended_way.py
+++ python_challenge/rotate_string_recommended_way.py
@@ -14,6 +14,3 @@ with open('problem2_encrypted_message.txt') as file:
 n_rotations = 2
 print(f'The rotated form of\n\n{string_to_be_rotated}\n\nis\n\n{rotate_by_n_chars(string_to_be_rotated, n_rotations)}')
 print(f'\nThe sring was rotated by {n_rotations}')
-
-# 97 to 122 is lowercase letter ASCII
-# 65 to 90  is uppercase letter ASCII
