90b175b0e09b5e6edbe0c633fdff2d1084423a0c
Arjan Gupta <arjangupta95@gmail.com>
2019-05-30 14:23:57 -0500

    First flask program

---
+++ flask/app.py
@@ -0,0 +1,14 @@
+# Arjan's first Flask program
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
