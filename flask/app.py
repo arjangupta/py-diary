336f2a67ec301ef2584754d46510b74d757cf215
Arjan Gupta <arjangupta95@gmail.com>
2019-06-03 16:45:27 -0500

    Render a webpage for tacos

--- flask/app.py
+++ flask/app.py
@@ -4,13 +4,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+num_tacos = 0
+
 @app.route('/')
 def index(): #this function can be named anything. It defines the route of the landing page, i.e. '/'
     return render_template("index.html")
 
 @app.route('/tacos')
 def tacos():
-    return 'Tacos are yummy'
+    return render_template("tacos.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') #0.0.0.0 means that this web app is accessible to any device on the current network
