0ad22b2b95f91b46c8ebc0a3ed7baf4fc5da035e
Arjan Gupta <arjangupta95@gmail.com>
2019-06-04 16:25:38 -0500

    Improve naming for taco amount, including a global. Also allow GET and POST methods

--- flask/app.py
+++ flask/app.py
@@ -10,9 +10,12 @@ num_tacos = 0
 def index(): #this function can be named anything. It defines the route of the landing page, i.e. '/'
     return render_template("index.html")
 
-@app.route('/tacos')
+@app.route('/tacos', methods=['POST', 'GET'])
 def tacos():
-    return render_template("tacos.html", num_tacos=num_tacos)
+    global num_tacos
+    print("You have hit the tacos endpoint")
+    num_tacos += 1
+    return render_template("tacos.html", tacos_requested=num_tacos)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') #0.0.0.0 means that this web app is accessible to any device on the current network
