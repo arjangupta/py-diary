ecc67b6ef0a06f69d44723fa8ff62b764e7cf521
Arjan Gupta <arjangupta95@gmail.com>
2019-08-19 03:01:38 -0500

    Use sys to use the first arg to prog as input file

--- small_programs/write_zeros.py
+++ small_programs/write_zeros.py
@@ -1,11 +1,15 @@
+import sys
+
 def main():
-    f = open("file.txt", "r")
-    g = open("file_corrected.txt", "w")
+    input_file_name = sys.argv[1]
+    f = open(input_file_name, "r")
+    output_file = input_file_name.replace("output", "output2", 1)
+    g = open(output_file, "w")
     corrected_line = ""
     if f.mode == "r" and g.mode == "w":
         lines = f.readlines()
         for line in lines:
-            line = line.replace(" 1 ", " 0 ", 1)
+            line = line.replace(", ...", "", 1)
             g.write(line)
     f.close()
     g.close()
