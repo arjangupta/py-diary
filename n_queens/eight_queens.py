13df249c9f775dd8fc5211fe7e700a7a9500613d
Arjan Gupta <agupta@intellifarms.com>
2019-11-18 10:56:26 -0600

    Create an eight queens puzzle program

---
+++ n_queens/eight_queens.py
@@ -0,0 +1,13 @@
+def main():
+    pass
+
+class QueenChessBoard:
+    def place_queen(self, alpha, num):
+        # Place a queen here
+
+    def check_square(self, alpha, num):
+        # Check if this square is in an already placed queen's path
+
+
+if __name__ == "__main__":
+    main()
