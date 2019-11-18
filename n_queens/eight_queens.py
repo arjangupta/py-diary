4377b2d3058b6260bc3899a9b2160fdd1a72ae5f
Arjan Gupta <agupta@intellifarms.com>
2019-11-18 11:23:34 -0600

    Create some objects for chess board/square

--- n_queens/eight_queens.py
+++ n_queens/eight_queens.py
@@ -1,11 +1,23 @@
 def main():
     pass
 
+class ChessSquare:
+    def __init__(self, alpha, num):
+        self.alpha = alpha
+        self.num   = num
+
 class QueenChessBoard:
     def place_queen(self, alpha, num):
         # Place a queen here
 
     def check_square(self, alpha, num):
+        # Check if alpha, num is even valid
+        if alpha not in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+            return False
+        
+        if num not in list(range(1:9)):
+            return False
+
         # Check if this square is in an already placed queen's path
 
 
